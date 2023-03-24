5bbc1ba67561c2e158bef8096f514dee0f8fde4e
vivektarun <vivektarun1234@gmail.com>
2023-03-24 23:42:26 +0530

    working properly before using express validator

--- backend/Routes/CreatUser.js
+++ backend/Routes/CreatUser.js
@@ -1,21 +1,32 @@
-const { response } = require('express')
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
 
-router.post("/creatuser", async (req, res) => {
-    try {
-       await User.create({
-            name: req.body.name,
-            password: req.body.password,
-            email: req.body.email,
-            location: req.body.location
-        })
-    response.json({success:true})    
-    } catch (error) {
-        console.log(error);
-        res.json({success:false})
-    }
-})
+
+
+
+router.post("/creatuser", 
+    async (req, res) => {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        res.send(req.body);
+
+        try {
+            await User.create({
+                name: req.body.name,
+                password: req.body.password,
+                email: req.body.email,
+                location: req.body.location
+            })
+            response.json({ success: true })
+        } catch (error) {
+            console.log(error);
+            res.json({ success: false })
+        }
+    })
 
 module.exports = router;
